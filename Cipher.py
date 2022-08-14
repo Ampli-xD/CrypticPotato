066e37aeb0b929b3957af249c59f8b83dc4c3f18
Dishant Kapoor <dishantkapoor29@gmail.com>
2022-08-14 22:11:40 +0530

    file setup

--- Cipher.py
+++ Cipher.py
@@ -1,7 +1,50 @@
-from main.py import *
-Inp = input("Enter the text to be converted: ").upper().replace(" ","/")
-Key = input("Enter the key: ")
-Initialize(len(Inp))
-pair(Inp, Key)
+from string import *
+B_Map=[]
+B_Key=[]
+code_pair=[]
+LinkedLists={}
+global loop1
+loop1=False
+global num
+num=0
+def Initialize(length):
+  x=0
+  while x!=10000:
+    for i in ascii_uppercase+digits:
+      B_Map.append(i)
+    x+=1
+#  return B_Ma
+  return B_Map
 
-for i in code_pair: print(i)
+
+def pair(Inp, Key):
+  global loop1
+
+  global num
+  for i in Inp:
+    if i == "/":
+      code_pair.append(("*",i))
+    else:
+      
+      if num != len(Key):
+        loop1=True
+        while loop1 is True:
+          code_pair.append((Key[num],i))
+          num=num+1
+          loop1 = False
+      else:
+        num=0
+        loop1=True
+        while loop1 is True:
+          code_pair.append((Key[num],i))
+          num=num+1
+          loop1 = False
+        
+  return code_pair
+
+
+
+
+
+
+
--- main.py
+++ main.py
@@ -1,53 +1,7 @@
-from string import *
-B_Map=[]
-B_Key=[]
-code_pair=[]
-LinkedLists={}
-global loop1
-loop1=False
-global num
-num=0
-def Initialize(length):
-  x=0
-  while x!=10000:
-    for i in ascii_uppercase+digits:
-      B_Map.append(i)
-    x+=1
-#  return B_Ma
-  return B_Map
+from Cipher import *
+Inp = input("Enter the text to be converted: ").upper().replace(" ","/")
+Key = input("Enter the key: ")
+Initialize(len(Inp))
+pair(Inp, Key)
 
-
-def pair(Inp, Key):
-  global loop1
-
-  global num
-  for i in Inp:
-    if i == "/":
-      code_pair.append(("*",i))
-    else:
-      
-      if num != len(Key):
-        loop1=True
-        while loop1 is True:
-          code_pair.append((Key[num],i))
-          num=num+1
-          loop1 = False
-      else:
-        num=0
-        loop1=True
-        while loop1 is True:
-          code_pair.append((Key[num],i))
-          num=num+1
-          loop1 = False
-        
-  return code_pair
-
-
-def LinkLists(Key):
-  for i in ascii_uppercase:
-    for j in Key:
-      LinkedLists=()
-
-
-
-
+for i in code_pair: print(i)
