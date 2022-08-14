d9e8053989a68ddcaf922b361baa8cc9695456cd
Dishant Kapoor <dishantkapoor29@gmail.com>
2022-08-14 22:20:42 +0530

    .

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from Cipher import *
 Inp = input("Enter the text to be converted: ").upper().replace(" ","/")
 Key = input("Enter the key: ")
-Initialize(len(Inp))
+#Initialize(len(Inp))
 pair(Inp, Key)
 
 for i in code_pair: print(i)
