077f818b9ecc6ad5b782c6fafaf4aab4714cf397
Dishant Kapoor <dishantkapoor29@gmail.com>
2022-08-14 22:03:29 +0530

    file shift

--- Cipher.py
+++ Cipher.py
@@ -1,1 +1,7 @@
 from main.py import *
+Inp = input("Enter the text to be converted: ").upper().replace(" ","/")
+Key = input("Enter the key: ")
+Initialize(len(Inp))
+pair(Inp, Key)
+
+for i in code_pair: print(i)
--- main.py
+++ main.py
@@ -51,9 +51,3 @@ def LinkLists(Key):
 
 
 
-Inp = input("Enter the text to be converted: ").upper().replace(" ","/")
-Key = input("Enter the key: ")
-Initialize(len(Inp))
-pair(Inp, Key)
-
-for i in code_pair: print(i)
